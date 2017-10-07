c58d770902ddc526b06d22099e0f4927c539a96d
Barakm <barakm@wix.com>
2017-10-07 08:40:29 +0300

    Logout service

---
+++ src/servlets/LogoutServlet.java
@@ -0,0 +1,25 @@
+package servlets;
+
+import webmanagers.SessionManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by barakm on 07/10/2017
+ */
+@WebServlet(name = "LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        SessionManager.removeUser(request.getParameter("username"));
+    }
+}
