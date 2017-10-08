a922337ed9379d1a804211c015f00c2adb5181d2
TalZamir <talzamir92@gmail.com>
2017-10-08 11:50:09 +0300

    Upload file initial

--- web/pages/js/Lobby.js
+++ web/pages/js/Lobby.js
@@ -15,7 +15,7 @@ function getOnlineUsers() {
             document.getElementById("users-list").innerHTML = result;
         },
         error: function (error) {
-            alert(error);
+            // alert(error);
         }
     });
 }
@@ -32,7 +32,7 @@ function getGames() {
             document.getElementById("games-list").innerHTML = result;
         },
         error: function (error) {
-            alert(error);
+            // alert(error);
         }
     });
 }
@@ -44,3 +44,35 @@ function ajaxCalls() {
     getOnlineUsers();
     getGames();
 }
+
+
+function bs_input_file() {
+    $(".input-file").before(
+        function () {
+            if (!$(this).prev().hasClass('input-ghost')) {
+                var element = $("<input type='file' class='input-ghost' style='visibility:hidden; height:0'>");
+                element.attr("name", $(this).attr("name"));
+                element.change(function () {
+                    element.next(element).find('input').val((element.val()).split('\\').pop());
+                });
+                $(this).find("button.btn-choose").click(function () {
+                    element.click();
+                });
+                $(this).find("button.btn-reset").click(function () {
+                    element.val(null);
+                    $(this).parents(".input-file").find('input').val('');
+                });
+                $(this).find('input').css("cursor", "pointer");
+                $(this).find('input').mousedown(function () {
+                    $(this).parents('.input-file').prev().click();
+                    return false;
+                });
+                return element;
+            }
+        }
+    );
+}
+
+$(function () {
+    bs_input_file();
+});
