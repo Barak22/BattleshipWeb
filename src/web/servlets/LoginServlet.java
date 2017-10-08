26a1c62387283beed8effb722b704c4125382747
Barakm <barakm@wix.com>
2017-10-08 10:37:42 +0300

    Add cookie

--- src/web/servlets/LoginServlet.java
+++ src/web/servlets/LoginServlet.java
@@ -4,30 +4,31 @@ import core.managers.SessionManager;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "LoginServlet")
 public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String userName = request.getParameter("username");
-        PrintWriter out = response.getWriter();
-        userName = userName.trim();
-        if (userName != null && !userName.isEmpty()) {
+        String userName = request.getParameter("username").trim();
+
+        if (!userName.isEmpty()) {
             if (SessionManager.addUser(userName) /*user name NOT already exists */) {
                 response.setStatus(200);
+                Cookie cookie = new Cookie("username", userName);
+                cookie.setPath("/");
+                response.addCookie(cookie);
             } else {
                 response.setStatus(400);
             }
         } else {
             response.setStatus(500);
         }
-
     }
 
     @Override
